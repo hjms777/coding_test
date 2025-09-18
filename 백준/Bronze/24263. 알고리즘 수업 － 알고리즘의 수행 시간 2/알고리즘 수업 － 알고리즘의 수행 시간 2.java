8c99cf2d37fa392c42b345a9497bc3eec63641c8
hjms777 <hjms777@naver.com>
2025-09-18 20:49:50 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 176 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       /*
+       MenOfPassion(A[], n) {
+            sum <- 0;
+            for i <- 1 to n
+                sum <- sum + A[i]; # 코드1
+            return sum;
+        }*/
+        // 1부터 n번 반복
+        // 다항식 f(n) = n
+        // f(n)의 최고차항 n¹
+
+        // 정리
+        // O(n) 알고리즘 결과는 항상 n
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+        System.out.println(1);
+
+
+    }
+}
